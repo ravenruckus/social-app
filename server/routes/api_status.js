16b08e2418517192c513c6ca3219ffd67f797e9f
ravenruckus <gyorihome@hotmail.com>
2017-02-25 13:55:23 -0800

    Close #4 Give status route ability to post, patch, & get by id

--- server/routes/api_status.js
+++ server/routes/api_status.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const knex = require('../db')
 
-router.get('/status', (_req, res) => {
+router.get('/', (_req, res) => {
   knex('status')
     .orderBy('updated_at', 'desc')
     .then((rows) => {
@@ -10,6 +10,68 @@ router.get('/status', (_req, res) => {
     }
   )
 })
+
+router.post('/', (req, res, next) => {
+
+  const { user_id, status_update, link } = req.body;
+  const insertStatus = {user_id};
+
+  if(status_update) {
+    insertStatus.status_update = status_update;
+  }
+
+  if(link) {
+    insertStatus.link = link;
+  }
+
+  knex('status')
+    .insert(insertStatus, '*')
+    .then((rows) => {
+      const status= rows[0];
+      res.send(status);
+    })
+})
+
+router.patch('/:id', (req, res, next) => {
+  const id = Number.parseInt(req.params.id);
+
+  knex('status')
+    .where('id', id)
+    .first()
+    .then((status) => {
+
+
+      const { status_update, link } = req.body;
+      const updateStatus = {};
+
+      if(status_update) {
+        updateStatus.status_update = status_update;
+      }
+
+      if(link) {
+        updateStatus.link = link;
+      }
+      return knex('status')
+        .update(updateStatus, '*')
+        .where('id', id);
+    })
+    .then((rows) => {
+      const status = rows[0];
+
+      res.send(status);
+    })
+})
+
+router.get('/:id', (req, res, next) => {
+  const id = Number.parseInt(req.params.id);
+
+  knex('status')
+    .where('id', id)
+    .first()
+    .then((row) => {
+      res.send(row);
+    })
+});
 
 router.get('/', (req, res) => {
   res.send('Hi from STATUS API')
