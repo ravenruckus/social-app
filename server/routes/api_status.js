34c982cebe52abb7f4fe9f38e59a7ecb148ffafb
ravenruckus <gyorihome@hotmail.com>
2017-02-25 13:14:46 -0800

    Close #3 Make route to get all status ordered by updated_at

--- server/routes/api_status.js
+++ server/routes/api_status.js
@@ -1,6 +1,16 @@
 const router = require('express').Router()
 const knex = require('../db')
 
+router.get('/status', (_req, res) => {
+  knex('status')
+    .orderBy('updated_at', 'desc')
+    .then((rows) => {
+       console.log(rows)
+      res.send(rows);
+    }
+  )
+})
+
 router.get('/', (req, res) => {
   res.send('Hi from STATUS API')
 })
