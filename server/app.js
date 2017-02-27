b42a1caa5b5913710ee02303b798f87089fbf160
ravenruckus <gyorihome@hotmail.com>
2017-02-27 11:19:07 -0800

    Close #6 Add error handling

--- server/app.js
+++ server/app.js
@@ -11,5 +11,23 @@ app.use('/api', require('./routes/api'))
 app.use('/api/projects', require('./routes/api_projects'))
 app.use('/api/status', require('./routes/api_status'))
 
+app.use((_req, res) => {
+  res.sendStatus(400)
+})
+
+// eslint-disable-next-line max-params
+app.use((err, _req, res, _next) => {
+  if (err.output && err.output.statusCode) {
+    return res
+      .status(err.output.statusCode)
+      .set('Content-Type', 'text/plain')
+      .send(err.message)
+  }
+
+  //eslint-disable-next-line no-console
+  console.error(err.stack)
+  res.sendStatus(500)
+})
+
 
 module.exports = app;
--- server/routes/api_status.js
+++ server/routes/api_status.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 const knex = require('../db')
+const boom = require('boom')
+
 
 router.get('/', (_req, res) => {
   knex('status')
@@ -7,8 +9,10 @@ router.get('/', (_req, res) => {
     .then((rows) => {
        console.log(rows)
       res.send(rows);
-    }
-  )
+    })
+    .catch((err) => {
+      next(err)
+    })
 })
 
 router.post('/', (req, res, next) => {
@@ -30,15 +34,25 @@ router.post('/', (req, res, next) => {
       const status= rows[0];
       res.send(status);
     })
+    .catch((err) => {
+      next(err)
+    })
 })
 
 router.patch('/:id', (req, res, next) => {
   const id = Number.parseInt(req.params.id);
 
+  if (Number.isNaN(id)) {
+    return next()
+  }
+
   knex('status')
     .where('id', id)
     .first()
     .then((status) => {
+      if (!status)  {
+        throw boom.create(404, 'Not Found')
+      }
 
 
       const { status_update, link } = req.body;
@@ -60,10 +74,17 @@ router.patch('/:id', (req, res, next) => {
 
       res.send(status);
     })
+    .catch((err) => {
+      next(err)
+    })
 })
 
 router.delete('/:id', (req, res, next) => {
   const id = Number.parseInt(req.params.id);
+
+  if (Number.isNaN(id)) {
+    return next()
+  }
 
   knex('status')
     .del('*')
@@ -71,9 +92,16 @@ router.delete('/:id', (req, res, next) => {
     .then((rows) => {
       let status = rows[0];
 
+      if (!status) {
+        return next()
+      }
       delete status.id;
       res.send(status);
-    });
+    })
+    .catch((err) => {
+      console.log(err)
+      next(err)
+    })
 });
 
 router.get('/:id', (req, res, next) => {
@@ -83,7 +111,13 @@ router.get('/:id', (req, res, next) => {
     .where('id', id)
     .first()
     .then((row) => {
+      if (!row) {
+        throw boom.create(404, 'Not Found')
+      }
       res.send(row);
+    })
+    .catch((err) => {
+      next(err)
     })
 });
 
@@ -95,6 +129,7 @@ router.post('/:id/likes', (req, res, next) => {
     .then((status) => {
       res.json({likes: status.likes})
     })
+    .catch(err => next(err))
 })
 
 router.delete('/:id/likes', (req, res, next) => {
@@ -105,7 +140,12 @@ router.delete('/:id/likes', (req, res, next) => {
     .then((status) => {
       res.json({likes: status.likes})
     })
+    .catch(err => next(err))
 })
+
+// router.get('/:status_id/comments')
+//   knex('status_comments')
+//     .insert({status_id: status_id, user_id: user_id, status: status })
 
 router.get('/', (req, res) => {
   res.send('Hi from STATUS API')
