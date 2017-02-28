a39a4796e9c686c1cb852f9839ab00c0e0439173
ravenruckus <gyorihome@hotmail.com>
2017-02-27 17:11:48 -0800

    Close #13 Create api for timeline

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,15 +1,28 @@
 const router = require('express').Router()
 const knex = require('../db')
+const boom = require('boom')
+const { camelizeKeys, decamelizeKeys } = require('humps')
 
-router.get('/users', (req, res) => {
-  knex('users')
-    .then((response) => {
 
-      res.send(response);
-
+router.get('/timeline', (req, res) => {
+  knex('status')
+    .orderBy('updated_at', 'desc')
+    .then((rows) => {
+      const timeline = camelizeKeys(rows)
+      knex('projects')
+        .orderBy('updated_at', 'desc')
+        .then((projectRows) => {
+          const combined = timeline.concat(camelizeKeys(projectRows))
+          console.log(combined)
+          combined.sort(function (a, b) {
+            return b.updatedAt - a.updatedAt
+          })
+          res.send(combined)
+        })
     })
-
 })
+
+
 
 router.get('/test', (req, res) => {
   res.send('Hi from API')
